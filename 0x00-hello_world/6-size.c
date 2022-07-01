f6706fe899bc2caa85f10bc0a34ca2c50c8da0c5
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-01 04:40:53 -0700

    edited indent

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,12 @@ int main(void)
 	float d;
 	char c;
 
-	printf("Size of a Char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-        printf("Size of a float: %lu byte(s)\n", sizeof(d));
-return (0);
+printf("Size of a Char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+printf("Size of a float: %lu byte(s)\n", sizeof(d));
+       
+       return (0);
+
 }
