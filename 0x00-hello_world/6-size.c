d77ba33a0961d8d42038dc6ce7df4b43ec05b8a6
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-01 05:36:40 -0700

    collaboration is the best beauty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int ll;
 	float d;
 	char c;
-printf("Size of a Char: %lu byte(s)\n", sizeof(c));
-printf("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-printf("Size of a float: %lu byte(s)\n", sizeof(d));
+printf("Size of a Char: %zu byte(s)\n", sizeof(c));
+printf("Size of an int: %zu byte(s)\n", sizeof(i));
+printf("Size of a long int: %zu byte(s)\n", sizeof(l));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(ll));
+printf("Size of a float: %zu byte(s)\n", sizeof(d));
 return (0);
 }
