b1da5600b6ebdd1229d34b7eb3c3c7493820f6c6
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-01 14:36:35 -0700

    I love codding in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,10 +11,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
+}
 else if (n > 0)
-printf("%d is positive\n", n);
+{
+	printf("%d is positive\n", n);
+}
 else
-printf("%d is zero\n", n)
+{
+	printf("%d is zero\n", n);
+}
 return (0);
 }
