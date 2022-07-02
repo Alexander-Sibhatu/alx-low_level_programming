5f16f04c6f4e54db9249785bfece20188fc9b3e6
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 11:05:46 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints digit numbers of base 10
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+int i, j;
+for (i = 10; i <= j*10; i++)
+{
+	if (j <= 9)
+	{
+	printf("%i", i);
+	}
+printf("\n");
+return (0);
+}
