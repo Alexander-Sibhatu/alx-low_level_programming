ffe44804e181edbb7bac9c087fb218873d20e14f
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 10:36:51 -0700

    a code to print all alphabets but 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.he>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints lowercase alpha with exception letters
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	if (ch == 'q' || ch == 'e')
+	{
+		printf("");
+	}
+	printf("%c", ch);
+}
+return (0);
