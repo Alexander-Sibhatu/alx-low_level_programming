794d82979ba424887413a20cc1b75f09e6e55dcb
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 05:21:39 -0700

    semicolon ';' added after each statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,16 +11,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-	pritnf("%d and is greater thatn 5\n", n)
+	printf("%d and is greater thatn 5\n", n);
 }
 else if (n == 0)
 {
-	printf("%d and is 0\n", n)
+	printf("%d and is 0\n", n);
 }
 
-else(n < 6 && n != 0)
+else(n<6 && n!=0)
 {
-	printf("%d and is less than 6 and not 0\n", n)
+	printf("%d and is less than 6 and not 0\n", n);
 }
 
 	return (0);
