6a6e84372fdeb42c3029529ab9bbd5013e91cf1c
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 15:27:42 -0700

    smile in the mirror reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints the lowercase alphabet in reverse
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
+
