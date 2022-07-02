efdf4afa89f165dd5ce0f285408637c8d049004e
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 06:01:44 -0700

    little spelling error fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ n = rand() - RAND_MAX / 2;
 
 if ((n%10) > 5)
 {
-	printf("Last digit of %d is %d and is greater thatn 5\n", n, n%10);
+	printf("Last digit of %d is %d and is greater than 5\n", n, n%10);
 }
 else if ((n%10) == 0)
 {
