6a4da1950be320f1be978581d9040539aeb448a7
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 06:00:37 -0700

    n%10 is importatn command

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,18 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int dig = n % 10;
-if (n > 5)
+
+if ((n%10) > 5)
 {
-	printf("Last digit of %d is %d and is greater thatn 5\n", n, dig);
+	printf("Last digit of %d is %d and is greater thatn 5\n", n, n%10);
 }
-else if (n == 0)
+else if ((n%10) == 0)
 {
-	printf("Last digit of %d is %d and is 0\n", n, dig);
+	printf("Last digit of %d is %d and is 0\n", n, n%10);
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, dig);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
 }
 
 	return (0);
