5216ffecf67436eae74411f7a133263260f4e7c5
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 16:23:27 -0700

    this issi it

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all the possible different combinations of two digits.
+ *
+ * Return: (0) Success
+ */
+int main(void)
+int i, e;
+i = 48;
+e = 48;
+
+while (e < 58)
+{
+	i = 48;
+	while (i < 58)
+	{
+		if (e != i && e < i)
+		{
+			putchar(e);
+			putchar(i);
+			if (i == 57 && e == 56)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	e++;
+}
+putchar('\n');
+return (0);
+}
