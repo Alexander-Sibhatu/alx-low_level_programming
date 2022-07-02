d6597d9744d3610ef95e9785fb87f6d5ea7f3ab2
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 15:01:19 -0700

    Numberz, a program that prints all single numbers of base 10 starting from 0.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: single digit numbers of base 10
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+	putchar(i);
+}
+printf("\n");
+return (0);
+}
+
