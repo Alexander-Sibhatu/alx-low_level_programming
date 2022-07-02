8cc316975faacca75d8e1de8ab4bf6989beb79b9
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-02 06:40:59 -0700

    alphabets lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this code displays lower case alphabets from a to z
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for(ch = 'a'; ch <= 'z'; chr++)
+{
+	printf("%d", ch)
+	putchar (ch);
+	putchar ('\n');
+	getch();
+}
