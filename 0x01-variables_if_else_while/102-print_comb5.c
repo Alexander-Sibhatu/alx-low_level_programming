acfed19d7eaa0fa17fd47a18f06dcc202435b6b8
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-03 04:17:00 -0700

    combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Description: a program that prints all combinations of two two-digit num
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+	for (j = 0; j < 100; j++)
+	{
+		if (i < j)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if ( i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
