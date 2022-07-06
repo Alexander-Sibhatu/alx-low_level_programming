f57754541513233f63f7595a0299ad8dfdfddde2
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-06 06:18:16 -0700

    test _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,27 @@
 #include "main.h"
 #include<stdio.h>
+
 /**
- * _islower  - check the code.
+ * test_islower - Test the _islower function
  *
- * parameter c: is used as an argument
+ * @n: Number to pass to _islower function
+ */
+void test_islower(int n)
+{
+	int r;
+
+	r = _islower(n);
+	_putchar(r + '0');
+	_putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int _islower(int c)
+int main(void)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (0);
-	}
-	else if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
+	test_islower('H');
+	return (0);
 }
