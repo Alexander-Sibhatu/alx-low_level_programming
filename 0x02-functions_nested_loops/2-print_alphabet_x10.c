d8db42688c94ec8156c9649d8aacef80269ffb1c
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-06 03:12:25 -0700

    the initial commit for file print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * 2-print_alphabet_x10.c - prints the alphabet 10 times.
+ *
+ * Description: includes the main.h file
+ *
+ * Return: void
+ */
+void 2-print_alphabet_x10.c(void)
+{
+	int i = 0;
+	while (i < =10)
+	{
+		int j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+	}
+}
