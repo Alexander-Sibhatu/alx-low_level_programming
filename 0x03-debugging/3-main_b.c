ef9430a83897164ed720b2cc72ebe9a04c8d879a
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-08 13:26:29 -0700

    the operators

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -9,15 +9,15 @@
 * @year: year
 * Return: void
 */
+
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || !(year % 4 == 0))
+	if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
