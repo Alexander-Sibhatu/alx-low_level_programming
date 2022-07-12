8554a4efc02e16b8f6ce9feaa584de81cbbc7e85
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-12 03:18:13 -0700

    betty doc style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int and updates the val it points to 98
  *
- * @n - is a pointer parameter
+ * @n: is a pointer parameter
  */
 
 void reset_to_98(int *n)
