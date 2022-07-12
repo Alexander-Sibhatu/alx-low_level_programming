e117f47d6400beb9344f3aab4b78944533922219
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-12 03:50:36 -0700

    3--uts.c fun written

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ *
+ * @str: parameter
+ *
+ * return:
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] != '\0')
+		{
+			_putchar(str[count]);
+			count++;
+		}
+		else
+		{
+			count = -1;
+			_putchar('\n');
+		}
+	}
+}
