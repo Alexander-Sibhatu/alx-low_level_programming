aa6eafe1aeccc7bd38a91783ccd149c5e0437ef1
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-12 05:01:45 -0700

    6-puts.c fun written

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string, starting w-  first character
  *
  * @str: parameter passed
  */
