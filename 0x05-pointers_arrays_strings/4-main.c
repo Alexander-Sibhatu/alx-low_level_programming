500cb7945eb4a7ae58a12f653f76613d61857694
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-14 10:15:33 -0700

    Task Done

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always o.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Issac Asimov";
+	print_rev(str);
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * *_strcat - concatenates two strings.
+ *
+ * @dest: char parameter
+ * @*src: char parameter
+ *
+ * Return: always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+
