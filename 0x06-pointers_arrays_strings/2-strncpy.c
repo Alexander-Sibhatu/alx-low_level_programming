164b329163f81a3eb33b76b0868927094ef599f1
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-14 16:02:35 -0700

    hot betty style fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings,
+ * _strncpy - concatenates two strings,
  * @dest: destination.
  * @src: source.
  * @n: amount of bytes used from src.
