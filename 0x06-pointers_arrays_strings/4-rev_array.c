0c1df360c1dcd651683c5adad80a6d9f961a9463
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-15 03:40:44 -0700

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,13 @@
 void reverse_array(int *a, int n)
 {
 	int i, j, k;
-	for (i = 0; i < n-1; i++)
+
+	for (i = 0; i < n - 1; i++)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
-			k = *(a + 1);
-			*(a + 1) = *(a + (j - 1));
+			k = *(a + j);
+			*(a + j) = *(a + (j - 1));
 			*(a + (j - 1)) = k;
 		}
 	}
