0846cfd38b42e8f1fa223910f64ae9911d8f14c3
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-07-29 02:54:31 -0700

    task done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, usigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
