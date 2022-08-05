fbc6e6bb17c29b24b2ad07728f45efdda2c1365b
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-08-05 03:14:05 -0700

    hot fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,7 +5,10 @@
  * File: variadic_functions.h
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0F-variadic_functions directory.
- *
+ * @n: number of arguments.
+ * @...: A variable number of parameters
+ * Return: 0 if n == 0.
+ *         sum if otherwise.
  */
 
 #include <stdarg.h>
