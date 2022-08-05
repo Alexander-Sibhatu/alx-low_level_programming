5095d013b62d8f59fcb3e97ee9aea648685037de
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-08-05 04:18:18 -0700

    hot fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@
  * @...: A variable number of numbers to be printed.
  */
 
-void print_ numbers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
