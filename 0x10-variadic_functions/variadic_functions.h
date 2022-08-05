fae35361751c460297996a8a4081c4340fc52450
Alexander Sibhatu <sibhatualexander5@gmail.com>
2022-08-05 11:32:04 -0700

    hot fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,5 +15,6 @@
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_srings(const char *separator, const unsigned int n, ...);
 
 #endif
